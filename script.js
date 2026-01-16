b95e1327831547ce86c1454f9ad4393907db1aa7
Seth Martineau <martineausw@gmail.com>
2026-01-16 13:34:58 -0500

    Refactor script to implement UI

--- script.js
+++ script.js
@@ -1,51 +1,109 @@
-function convertChoiceToString(choice) {
-    switch (choice) {
-        case 0: return "rock";
-        case 1: return "paper";
-        case 2: return "scissors";
-        default: return "INVALID";
-    }
-}
-function getPlayerChoice() {
-    const inputString = prompt("Enter rock, paper, or scissors").toLowerCase();
-    switch (inputString) {
-        case "rock": return 0;
-        case "paper": return 1;
-        case "scissors": return 2;
-        default: return getPlayerChoice();
-    }
-}
-
 function getComputerChoice() {
     return Math.floor(Math.random() * 3);
 }
 
-function playRound(playerChoice, computerChoice) {
+function playRound(event) {
+    if (isGameInProgress) {
+        let playerChoice = event.currentTarget.playerChoice;
+        let computerChoice = getComputerChoice();
+        console.log("Player", playerChoice);
+        console.log("Computer", computerChoice);
+        evalRound(playerChoice, computerChoice);
+    }
+    displayPlayerScore();
+    displayComputerScore();
+    isGameInProgress = evalGame(playerScore, computerScore);
+}
+
+function evalRound(playerChoice, computerChoice) {
     switch (playerChoice) {
         case computerChoice: 
-            alert("Computer chose " + convertChoiceToString(computerChoice) +".\nThe round is tied!");
-            return 0;            
+            displayGameOutcome("It's a tie!");
+            break;
         case (computerChoice + 1) % 3: 
-            alert("Computer chose " + convertChoiceToString(computerChoice) +".\nYou lost this round!");
-            return -1; 
+            incrementPlayerScore();
+            displayGameOutcome("You win the round!");
+            break;
         case (computerChoice + 2) % 3: 
-            alert("Computer chose " + convertChoiceToString(computerChoice) +".\nYou won this round!");
-            return 1; 
+            incrementComputerScore();
+            displayGameOutcome("You lose the round!");
+            break;
     }
 }
 
-function playGame() {
-    let score = 0;
-    for (let i = 0; i < 5; i++) {
-        score += playRound(getPlayerChoice(), getComputerChoice());
+function evalGame() {
+    if (getPlayerScore() == 5) {
+        displayGameOutcome("You win the game!");
+        return false;
+    } 
+    if (getComputerScore() == 5) {
+        displayGameOutcome("You lose the game!");
+        return false;
     }
-    if (score == 0) {
-        alert("The game is tied!");
-    } else if (score > 0) {
-        alert("You won the game!");
-    } else {
-        alert("You lost the game!");
-    }
+    return true;
 }
 
-playGame();
+
+function incrementPlayerScore() {
+    playerScore += 1;
+}
+
+function incrementComputerScore() {
+    computerScore += 1;
+}
+
+function getPlayerScore() {
+    return playerScore;
+}
+
+function getComputerScore() {
+    return computerScore;
+}
+
+function handleRock() {
+    let rockButtonElement = document.querySelector("#rock");
+    rockButtonElement.playerChoice = 0;
+    rockButtonElement.computerChoice = getComputerChoice();
+    rockButtonElement.addEventListener("click", playRound);
+}
+
+function handlePaper() {
+    let paperButtonElement = document.querySelector("#paper");
+    paperButtonElement.playerChoice = 1;
+    paperButtonElement.computerChoice = getComputerChoice();
+    paperButtonElement.addEventListener("click", playRound);
+}
+
+function handleScissors() {
+    let scissorsButtonElement = document.querySelector("#scissors");
+    scissorsButtonElement.playerChoice = 2;
+    scissorsButtonElement.computerChoice = getComputerChoice();
+    scissorsButtonElement.addEventListener("click", playRound);
+}
+
+function setButtonEventListeners() {
+    handleRock();
+    handlePaper();
+    handleScissors();
+}
+
+function displayGameOutcome(string) {
+    let gameOutcomeTextElement = document.querySelector("#gameOutcome");
+    gameOutcomeTextElement.textContent = string;
+}
+
+function displayPlayerScore() {
+    let playerScoreTextElement = document.querySelector("#playerScore");
+    playerScoreTextElement.textContent = getPlayerScore();
+}
+
+function displayComputerScore() {
+    let computerScoreTextElement = document.querySelector("#computerScore");
+    computerScoreTextElement.textContent = getComputerScore();
+}
+
+let playerScore = 0;
+let computerScore = 0;
+let isGameInProgress = true;
+
+setButtonEventListeners();
